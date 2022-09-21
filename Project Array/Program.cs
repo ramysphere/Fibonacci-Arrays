a6d559c65a52998f9e2da0c5605728c39801fe50
Hayder Sadig <Hayder Sadig@SADIG-HPLAPTOP>
2022-09-21 11:12:04 -0400

    Add project files.

---
+++ Project Array/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Project_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Compute the first 25 numbers of the Fibonacci Sequence");
+
+            int[] fibo = new int[25];
+            fibo[0] = 0;
+            fibo[1] = 1;
+
+        
+
+            for (int i = 0; i < 25; i++)
+            {
+                if (i >= 2) 
+
+                {
+                    fibo[i] = fibo[i - 2] + fibo[i - 1];
+                }
+
+                Console.WriteLine(fibo[i]);
+            }
+
+            
+        }
+    }
+}
